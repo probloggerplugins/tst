0bb82bc5db3e44ce8d912944a2da6fd74b5ac38c
probloggerplugins <73790652+probloggerplugins@users.noreply.github.com>
2021-04-24 22:11:41 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -6,15 +6,4 @@ async function huj() {
 	
 }
 
-faceapi.loadSsdMobilenetv1Model('https://probloggerplugins.github.io/tst/').then(() => {
-	const detections = faceapi.detectAllFaces(img)
-
-// resize the detected boxes in case your displayed image has a different size then the original
-const detectionsForSize = faceapi.resizeResults(detections, { width: img.width, height: img.height })
-// draw them into a canvas
-const canvas = document.createElement('canvas');
-document.body.appendChild(canvas);
-canvas.width = img.width
-canvas.height = img.height
-faceapi.drawDetection(canvas, detectionsForSize, { withScore: true })
-});
+faceapi.loadSsdMobilenetv1Model('https://probloggerplugins.github.io/tst/');
